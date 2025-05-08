9228a9edff08f12cc2a07c12756d9113f9259ad8
WangEntang <maxewang10@gmail.com>
2025-05-08 11:32:30 +0200

    5.8 assignment 4

---
+++ Assignment_4/exercise_2.py
@@ -0,0 +1,85 @@
+import math
+from collections import Counter
+from typing import List, Union
+
+from tokenizers import Tokenizer
+from morfessor.baseline import BaselineModel
+
+
+class TokenizerEntropy:
+    def tokenize_bpe(self, tokenizer: Tokenizer, text: str) -> List[str]:
+        """
+        Takes the BPE tokenizer and a text and returns the list of tokens.
+
+        params:
+        - tokenizer: The pre-trained BPE tokenizer
+        - text: The input text to tokenize
+
+        returns a list of tokens
+        """
+        # ====================================
+        # Your code here
+
+        # ====================================
+        raise NotImplementedError
+
+    def tokenize_morfessor(self, tokenizer: BaselineModel, text: str) -> List[str]:
+        """
+        Takes the Morfessor tokenizer and a text and returns the list of tokens.
+
+        params:
+        - tokenizer: The pre-trained Morfessor tokenizer
+        - text: The input text to tokenize
+
+        returns a list of tokens
+        """
+        # ====================================
+        # Your code here
+
+        # ====================================
+        raise NotImplementedError
+
+    def get_probs(self, tokens: List[str]):
+        """
+        Takes a list of tokens and compute the probability distribution of the tokens.
+
+        params:
+        - tokens: A list of tokens
+
+        returns a dictionary of token probabilities i.e. {token: probability, ...}
+        """
+        # ====================================
+        # Your code here
+
+        # ====================================
+        raise NotImplementedError
+
+    def compute_entropy(
+        self, text: str, tokenizer: Union[Tokenizer, BaselineModel]
+    ) -> float:
+        """
+        Takes the input text and the tokenizer and returns the entropy of the text.
+
+        params:
+        - text: The input text
+        - tokenizer: The pre-trained tokenizer (BPE or Morfessor)
+
+        returns the entropy of the text
+        """
+        # tokenize the input text
+        if isinstance(tokenizer, Tokenizer):
+            tokens = self.tokenize_bpe(tokenizer, text)
+        elif isinstance(tokenizer, BaselineModel):
+            tokens = self.tokenize_morfessor(tokenizer, text)
+        else:
+            raise ValueError("Tokenizer not supported.")
+
+        # ====================================
+        # Your code here
+
+        # get the probabilities of each token
+
+        # Compute the entropy
+
+        # ====================================
+        raise NotImplementedError
